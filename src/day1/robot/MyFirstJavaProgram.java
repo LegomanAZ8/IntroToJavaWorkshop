168721272bcdcbeea8431d36e5477fcd7bd7eef7
Jake Mitchell <legomanaz@gmail.com>
2016-06-28 14:56:59 -0700

    cool stuff i did

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,24 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot Alex = new Robot();
+for (int i = 4; i < args.length; i++) {
+Alex.
+Alex.move(1000);	
+Alex.turn(90);
+}
 
 	
-		
-		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 }
